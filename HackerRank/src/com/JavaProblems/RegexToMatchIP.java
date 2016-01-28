216db043d61a3e5092f4d964f90928c1484a4f30
jy2054050 <jy2054050@gmail.com>
2016-01-28 11:25:36 +0530

    hacker rank java probs added

---
+++ HackerRank/src/com/JavaProblems/RegexToMatchIP.java
@@ -0,0 +1,17 @@
+package com.New;
+
+import java.util.Scanner;
+
+public class RegexToMatchIP {
+
+	public static void main(String []args)
+    {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext())
+        {
+            String IP = in.next();
+            System.out.println(IP.matches(new myRegex().pattern));
+        }
+
+    }
+}
---
+++ HackerRank/src/com/JavaProblems/myRegex.java
@@ -0,0 +1,15 @@
+package com.New;
+
+
+public class myRegex {
+	public String pattern;
+
+/*static final String pattern1 ="^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+	static final Pattern pattern = Pattern.compile(pattern1);*/
+myRegex(){
+	  String zeroTo255 ="(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+      pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+
+  }
+}
+
