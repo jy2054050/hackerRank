f576f4b9af3a5d51e9ea4e82e5dd983516644f89
Jitender <jy2054050@gmail.com>
2016-02-06 16:15:20 +0530

    Height of a binary tree

--- HackerRank/src/com/Tree/HeightBinaryTree.java
+++ HackerRank/src/com/Tree/HeightBinaryTree.java
@@ -1,6 +1,6 @@
 package com.Tree;
 
-public class InorderTraversal {
+public class HeightBinaryTree {
 	public static void main(String[] args) {
 		Node one = new Node();Node two = new Node();Node three = new Node();
 		Node four = new Node();Node five = new Node();Node six = new Node();
@@ -11,11 +11,18 @@ public class InorderTraversal {
 		one.left=two; one.right=three; two.left=four;two.right =five; 
 		one.right=three; three.left=six;four.left=seven;
 		
-		Inorder(one);
+		System.out.println(height(one));
 	}
-	static void Inorder(Node root) {
-			if(root.left !=null) Inorder(root.left);
-			System.out.print(root.data+" ");
-			if(root.right !=null) Inorder(root.right);
+	static int height(Node root) {
+		if (root.left==null && root.right==null) return 1;
+		int l=0; int r=0;
+		if(root.left !=null){
+			l= height(root.left);	
+		}
+		
+		if(root.right !=null){
+			r=height(root.right);
+		}		
+		return Math.max(l,r)+1; 
 		}
 }
